815a3830f5fc3bdc8ba7d2340a0ad061b1234d78
knnthdmyo <donkennethgdemayo@gmail.com>
2025-01-01 20:17:51 +0800

    removed lint plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import eslintPlugin from 'vite-plugin-eslint';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/portfolio',
-  plugins: [react(), tsconfigPaths(), eslintPlugin()],
+  plugins: [react(), tsconfigPaths(), ],
 })
 
 
