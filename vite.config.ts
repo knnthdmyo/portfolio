ac224988b25c5df7b85ef9cd5482ea51806f616b
knnthdmyo <donkennethgdemayo@gmail.com>
2025-01-01 19:29:28 +0800

    update pkg.json with new nape

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/knnthdmyo.github.io/',
+  base: '/portfolio',
   plugins: [react(), tsconfigPaths()],
 })
