1871f48755887e1ce361afeebbaf30c875ef4d12
knnthdmyo <donkennethgdemayo@gmail.com>
2025-01-01 19:16:23 +0800

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/knnthdmyo.github.io/',
   plugins: [react(), tsconfigPaths()],
 })
